3c73d5f27d71873e30c241d9ec6c2f3212e9efbc
Coglab <cognition.lab@concordia.ca>
2021-07-27 13:29:40 +0200

    added id

--- 06_main.js
+++ 06_main.js
@@ -609,7 +609,7 @@ $("document").ready(function() {
         // Here, you can specify all information for the deployment
         deploy: {
             experimentID: "262",
-            serverAppURL: "https://magpie-demo.herokuapp.com/api/submit_experiment/",
+            serverAppURL: "https://magpie-demo.herokuapp.com/api/submit_experiment/262",
             // Possible deployment methods are:
             // "debug" and "directLink"
             // As well as "MTurk", "MTurkSandbox" and "Prolific"
